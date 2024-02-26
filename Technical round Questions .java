ff99c37b9f0189953c107f39f8ba5cf7f59cf715
Karthikeyan S <141199893+Karthikeyan1905@users.noreply.github.com>
2024-02-26 22:11:25 +0530

    Update Technical round Questions .java

--- Technical round Questions .java
+++ Technical round Questions .java
@@ -572,8 +572,48 @@ public class basics {
 output:
 
     10 20 36 41 54 
-    _________________________________________________________
+___________________________________________________________________________________________________
+14.Sorting n number using array.
     
+ code:
+    
+import java.util.Scanner;
+public class basics {
+    static  void Sorting(int [] arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[i]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+
+            }
+            System.out.print(arr[i]+"|");
+        }
+
+    }
+    public static void main(String[] args) {
+        int a[];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements you want in array:");
+        int size = sc.nextInt();
+        a = new int[size];
+        System.out.println("Enter " + size + " integers separated by space : ");
+        for (int i=0 ;i<size ; i++)  {
+            a[i] = sc.nextInt();
+        }
+        System.out.println(" before sorting : ");
+        for(int i=0 ; i<a.length; i++ ) {
+            System.err.println(a[i]);
+        }
+        System.out.println("\n after sorting : \n");
+        Sorting(a);
+        sc.close();
+    }
+}
+output:
+
 
 
 
