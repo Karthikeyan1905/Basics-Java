ac30997fcd93494ffbbaaede482de0a74d1c0983
Karthikeyan S <141199893+Karthikeyan1905@users.noreply.github.com>
2024-03-11 14:11:17 +0530

    Add files via upload

---
+++ src/pk1/Class1.java
@@ -0,0 +1,10 @@
+package pk1;
+
+import Access_Specifier.Modifier;
+
+public class Class1 {
+    public static void main(String[] args) {
+        Modifier caller =new Modifier();
+        caller.method1();
+    }
+}
