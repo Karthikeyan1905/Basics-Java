385aa5a5a257c6bf0aae18b1ac4016f0721f1cf1
Karthikeyan S <141199893+Karthikeyan1905@users.noreply.github.com>
2024-03-19 22:24:04 +0530

    Update Assignment.java

--- operator/Assignment.java
+++ operator/Assignment.java
@@ -4,7 +4,7 @@ public class Assignment {
     public static void main(String[] args) {
     int x = 10;
     int z = x;
-    System.out.println("y = " + z);
+    System.out.println("z = " + z);
 
     x += 5;
     System.out.println("x += 5: " + x);
@@ -23,3 +23,10 @@ public class Assignment {
 
 }
 }
+output:
+z = 10
+x += 5: 15
+x -= 3: 12
+x *= 2: 24
+x /= 4: 6
+x %= 3: 0
